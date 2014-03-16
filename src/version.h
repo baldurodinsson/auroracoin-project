2af4a12f2e4beffb5321eb8089e14a7ab065df9d
baldurodinsson <balduro@gmx.com>
2014-03-16 23:08:20 +0000

    Tweak hard fork @5400. Change version.
    Tweak hard fork @5400. Limit timejumping window.

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@
 
 // These need to be macro's, as version.cpp's voodoo requires it
 #define CLIENT_VERSION_MAJOR       1
-#define CLIENT_VERSION_MINOR       1
+#define CLIENT_VERSION_MINOR       2
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
@@ -33,7 +33,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 1010000;
+static const int PROTOCOL_VERSION = 1020000;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
